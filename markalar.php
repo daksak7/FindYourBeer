fb38e08d52120c5e4b63a994ec1b6d08f6ca77c2
Doğancan <53553597+daksak7@users.noreply.github.com>
2020-04-28 23:49:07 +0300

    added firebase codes

--- markalar.php
+++ markalar.php
@@ -1,4 +1,12 @@
 <?php include 'header.php'?>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.14.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
 <?php include 'connect.php'?>
         <link rel="stylesheet" href="css/cards.css">
         <link rel="stylesheet/less" type="text/css" href="css/styles.less" />

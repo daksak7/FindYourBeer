5ff9f3f59e1c7f9351b0cb1e318789607c479ea6
daksak7 <dogancan_aksak@hotmail.com>
2020-07-08 19:40:33 +0300

    Revert "added sub pages to JPLT page.Created level system"
    This reverts commit 3a484bad21737c4ff95025aeaee0dfad88e8cf7a.

--- index.php
+++ index.php
@@ -1,15 +1,80 @@
-<?php include 'firstload.php'?>
-<link rel="stylesheet" href="css/styles.css">
+<?php include 'header.php'?>
 
-  <body>
 
-</body>
-<style>
-  body {
-  background-image: url("img/login_background.jpg"); 
-  opacity: 0.90; 
-  background-size: 100%;}
-</style>
+<body>
+<div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-interval="2000">
+      <img src="img/slide02.jpg" style="height: 575px" class="d-block w-100" alt="Slayt 1">
+    </div>
+    <div class="carousel-item" data-interval="2000">
+      <img src="img/slide03.jpg"  style="height: 575px"class="d-block w-100" alt="Slayt 2">
+    </div>
+    <div class="carousel-item" data-interval="2000">
+      <img src="img/slide01.jpg" style="height: 575px"class="d-block w-100" alt="Slayt 3">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Geri</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">İleri</span>
+  </a>
+</div>
+
+    <div id="about" class="page-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-heading">
+                        <h4>WYBS Ne Sağlar?</h4>
+                        <div class="line-dec"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="service-item first-service">
+                        <div class="icon"></div>
+                        <h4>Anlaşılabilir Arayüz</h4>
+                        <p>WYBS çok basit bir arayüze sahiptir.Bu sayede kendine uygun olacak filtreleri kolaylıkla seçebilirsin.</p>
+                    </div>
+                </div>
+                 <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="service-item second-service">
+                        <div class="icon"></div>
+                        <h4>Geniş Bira Veritabanı</h4>
+                        <p>Hergün güncellenen bira arşivimiz sayesinde dünya çapındaki birçok bira le tanışma fırsatı bul.</p>
+                    </div>
+                </div>
+                 <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="service-item third-service">
+                        <div class="icon"></div>
+                        <h4>Ömür Boyu Ücretsiz</h4>
+                        <p>Sizlerden hiçbir şekilde ödeme talep edmiyoruz.Sadece reklam engelleyicilerinizi kapalı tutun o bize yeter :)</p>
+                    </div>
+                </div>
+                 <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="service-item fourth-service">
+                        <div class="icon"></div>
+                        <h4>Biraz da Hikaye</h4>
+                        <p>WYBS size sadece size uygun biraları sunmakla kalmayıp biraların çıkış yerlerinden tutun hikayelerine kadar birçok tarihsel bilgiyi de barındırıyor!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  	</body>
+
+
+
 
 
 <?php include 'footer.php'?>
+    
